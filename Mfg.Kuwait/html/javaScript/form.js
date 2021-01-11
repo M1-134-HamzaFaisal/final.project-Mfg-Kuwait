788d198fcc8601d46545c6c14b5e916cc56ede8d
M1-134-HamzaFaisal <76451177+M1-134-HamzaFaisal@users.noreply.github.com>
2021-01-11 12:21:34 +0300

    تم التعديل

--- Mfg.Kuwait/html/javaScript/form.js
+++ Mfg.Kuwait/html/javaScript/form.js
@@ -1,6 +1,8 @@
 function submit(){
-let email =document.getElementById(`email`).nodeValue;
-let Password =document.getElementById(`Password`).nodeValue;
+let email =document.getElementById(`email`).Value;
+let Password =document.getElementById(`Password`).Value;
 window.open(`mailto:${email}?subject=FromMfg&body${Password}`)
+
+
 
 }
